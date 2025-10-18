08064f5d8f67199ce27a49273c35e64b91be9841
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-10-17 21:49:34 -0400

    fixed error

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,7 +24,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+    public List<String> getSubBreeds(String breed) {
         if (breed == null) {
             throw new BreedNotFoundException("Breed not found");
         }
