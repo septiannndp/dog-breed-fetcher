2e6bd2663c4e18461d24a313586cf0c724350885
Septian Dimas Pasaribu <septianpasaribu1029@gmail.com>
2025-10-17 21:36:06 -0400

    finished task 3 and task 4

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -16,9 +16,8 @@ public interface BreedFetcher {
     List<String> getSubBreeds(String breed);
 
 
-    // TODO Task 4: make this a checked exception and update any other code as needed.
     // a class defined in an interface is public AND static
-    class BreedNotFoundException extends RuntimeException {
+    class BreedNotFoundException extends Exception {
         public BreedNotFoundException(String breed) {
             super("Breed not found: " + breed);
         }
